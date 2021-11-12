7b37ce9fb32220ffc38d00a23c40a470657a9b25
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2021-11-12 19:23:36 +0100

    Update Jan
    Interaction Patient script
    Material Invisible
    TMPro installiert

--- Healthcare Gamejam/Assets/Jan Scripts/InteractionPatient.cs
+++ Healthcare Gamejam/Assets/Jan Scripts/InteractionPatient.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractionPatient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject InteractionUI;
+    [SerializeField] TextMeshProUGUI InteractionUI_Name;
 
-    // Update is called once per frame
-    void Update()
+    /*private void Start()
     {
-        
+        InteractionUI = GameObject.Find("Interaction_UI");
+        InteractionUI_Name = GameObject.Find("Name_UI").GetComponent<TextMeshProUGUI>();
+    }*/
+
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Patient"))
+        {
+            Debug.Log("Needs recognized");
+            InteractionUI.SetActive(true);
+
+            //ask for name and set them
+            GameObject parent = other.transform.parent.gameObject;
+            string parentName = parent.name;
+            InteractionUI_Name.text = parentName;
+
+        }
     }
 }
