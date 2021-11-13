a9407b788c33f0c538c96cceaff162573a7d75e4
Uthak <81364780+Uthak@users.noreply.github.com>
2021-11-13 04:00:07 +0100

    Felix Update
    added post processing (not applied)
    added window screens
    fixed shadow problems
    updated level 1 to seem more inviting (2 doors gone)
    changed skybox for brighter lighting
    made beds jumpable
    made cat-patient prefab
    cat now animates properly
    cat should now stop and face player on contact
    returns to randomized walking on "continue" in game master
    added GameMaster script to GAME_MASTER
    added many pots and flowers

--- Healthcare Gamejam/Assets/Scripts/GameMaster.cs
+++ Healthcare Gamejam/Assets/Scripts/GameMaster.cs
@@ -1,18 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // controlls how long a character moves toward any given target
+    public float minTimerForNewPath = 3f;
+    public float maxTimerForNewPath = 8f;
 
-    // Update is called once per frame
-    void Update()
+    // used to stop a patient
+    public bool isBeingTalkedTo = false;
+
+    // in case we get around to playing sounds on clicking button
+    public AudioSource clickSound;
+    public AudioSource chatterSound;
+
+    // to open/close dialoge UI
+    [SerializeField] GameObject interaction_UI;
+
+    public void Continue()
     {
-        
+        StartCoroutine(ContinueSounds());
+        isBeingTalkedTo = false;
+        interaction_UI.SetActive(false);
+    }
+    IEnumerator ContinueSounds()
+    {
+        if (clickSound)
+        {
+            clickSound.Play();
+        }
+        yield return new WaitForSeconds(.5f);
+        if (chatterSound != null)
+        {
+            chatterSound.Play();
+        }
     }
 }
--- Healthcare Gamejam/Assets/Scripts/MoveRandomly.cs
+++ Healthcare Gamejam/Assets/Scripts/MoveRandomly.cs
@@ -17,28 +17,40 @@ public class MoveRandomly : MonoBehaviour
 
     NavMeshAgent navMeshAgent;
     NavMeshPath path;
-    [SerializeField] float minTimerForNewPath;
-    [SerializeField] float maxTimerForNewPath;
+    float minTimerForNewPath;
+    float maxTimerForNewPath;
     bool inCoRoutine;
     bool validPath;
     Vector3 target;
+    GameObject gameMaster;
+
+    // Animation Assets on character
+    Animation anim;
 
     void Awake()
     {
+        gameMaster = GameObject.Find("GAME_MASTER");
+
         navMeshAgent = GetComponent<NavMeshAgent>();
         path = new NavMeshPath();
 
-        // this part is to get cows Moving right on spawn
+        // this part is to get character moving right on spawn
         minTimerForNewPath = .1f;
         maxTimerForNewPath = .1f;
         Invoke("StartMoving", .5f);
+
+        // controls animations
+        anim = gameObject.GetComponent<Animation>();
+        StartCoroutine(CheckMoving());
     }
 
     // here the MeshNav receives its real time-variables
     void StartMoving()
     {
-        minTimerForNewPath = 3f;
-        maxTimerForNewPath = 8f;
+        minTimerForNewPath = gameMaster.GetComponent<GameMaster>().minTimerForNewPath;
+        maxTimerForNewPath = gameMaster.GetComponent<GameMaster>().maxTimerForNewPath;
+        //minTimerForNewPath = 3f;
+        //maxTimerForNewPath = 8f;
     }
 
     Vector3 getNewRandomPosition()
@@ -64,7 +76,6 @@ public class MoveRandomly : MonoBehaviour
         }
         inCoRoutine = false;
     }
-
     public void GetNewPath()
     {
         target = getNewRandomPosition();
@@ -72,9 +83,63 @@ public class MoveRandomly : MonoBehaviour
     }
     void Update()
     {
-        if (!inCoRoutine)
+        if (isOnTheMove)
+        {
+            anim.Stop("RabbitArmature_RabbitIdle");
+            anim.Play("RabbitArmature_RabbitWalking");
+        }else
+        {
+            anim.Stop("RabbitArmature_RabbitWalking");
+            anim.Play("RabbitArmature_RabbitIdle");
+        }
+
+        if (gameMaster.GetComponent<GameMaster>().isBeingTalkedTo == false && !inCoRoutine)
         {
             StartCoroutine(DoSomething());
         }
+
+        /*if (isBeingTalkedTo)
+        {
+            StopAndTalk(vector3 playerPos);
+        }*/
+    }
+    
+    bool isOnTheMove = false;
+    IEnumerator CheckMoving()
+    {
+        Vector3 startPos = transform.position;
+        yield return new WaitForSeconds(.1f);
+        Vector3 finalPos = transform.position;
+        
+        if(startPos != finalPos)
+        {
+            isOnTheMove = true;
+        }else
+        {
+            isOnTheMove = false;
+        }
+        StartCoroutine(CheckMoving());
+    }
+
+    //bool isBeingTalkedTo = false;
+
+    // Jan calls this when player moves close to patient
+    // hands over his position to look at
+    public void StopAndTalk(Vector3 playerPos)
+    {
+        // stop moving
+        gameMaster.GetComponent<GameMaster>().isBeingTalkedTo = true;
+        //isBeingTalkedTo = true;
+        
+        // look at player
+        gameObject.transform.forward = playerPos;
+
+        // stop patient
+        target = gameObject.transform.position;
+        inCoRoutine = false; // redundant?
+        //navMeshAgent.Stop(); // redundant?
+        //navMeshAgent.isStopped = true; // redundant??
+        // reset path 
+        navMeshAgent.ResetPath(); // redundant??
     }
 }
