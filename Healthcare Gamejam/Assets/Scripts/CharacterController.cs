916a0f40d241db0d4ba3aa6f11de8edf624d59fc
Uthak <81364780+Uthak@users.noreply.github.com>
2021-11-13 15:40:31 +0100

    Felix Update
    threw out shader
    hovering over walls makes them see thorugh and enables to click on area behind it for 1 sec
    character controller fully functional

--- Healthcare Gamejam/Assets/Scripts/CharacterController.cs
+++ Healthcare Gamejam/Assets/Scripts/CharacterController.cs
@@ -4,11 +4,16 @@ using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
-    public Vector3 GoToAllyPoint; // why tho
+    //public Vector3 GoToAllyPoint; // why tho
     NavMeshAgent agent;
+    //private GameObject cameraObject;
+    //private Camera mainCamera;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        //cameraObject = GameObject.Find("Main Camera");
+        //mainCamera = cameraObject.GetComponent<Camera>();
     }
     void Update()
     {
@@ -19,6 +24,76 @@ public class CharacterController : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
+            /* Debug.Log("step 1");
+
+             Transform targetTransform = null; // target assigned in inspector 
+             Debug.Log("step 2");
+
+             RaycastHit[] hits; // returned hits
+             Debug.Log("step 3");
+
+             LayerMask mask = new LayerMask(); // set in inspector
+             Debug.Log("step 4");
+
+             hits = Physics.RaycastAll(transform.position, transform.forward, Mathf.Infinity);
+             Debug.Log("step 5");
+
+             for (int i = 0; i < hits.Length; i++)
+             {
+                 Debug.Log("step 6");
+
+                 if (hits[i].collider.gameObject.CompareTag("Floor"))
+                 {
+                     Debug.Log("Found Floor!");
+
+                     // Found something with a given tag. 
+                 }
+             }
+
+             /*
+                 Debug.Log("step 1");
+
+             //RaycastHit hit;
+             RaycastHit[] hits;
+             Debug.Log("step 2");
+
+             LayerMask mask = new LayerMask();
+             hits = Physics.RaycastAll(cameraObject.transform.position, transform.forward, mask);
+             hits = Physics.RaycastAll(mainCamera.transform.position, transform.forward, Mathf.Infinity);
+             Debug.Log("step 3");
+
+
+             for (int i = 0; i < hits.Length; i++)
+             {
+                 Debug.Log("step 4");
+
+                 if (hits[i].collider.gameObject.CompareTag("Floor"))
+                 {
+                     Debug.Log("step 5");
+
+                     //agent.SetDestination(hit.point);
+                 }
+             }
+
+
+                 /*if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+                 {
+                     //GameObject hitObjct = hit.collider.gameObject;
+
+                     hit.collider.gameObject.GetComponent<Collider>().enabled = false; 
+
+                     agent.SetDestination(hit.point);
+                 }
+                 if (hit.collider.gameObject.CompareTag("Wall"))
+                 {
+                     hit.collider.gameObject.GetComponent<Collider>().enabled = false;
+                     RaycastHit newHit;
+                 }
+                 // old script
+                 if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+             {
+                 agent.SetDestination(hit.point);
+             }*/
         }
     }
 }
--- Healthcare Gamejam/Assets/Scripts/HoverSeeThroughWalls.cs
+++ Healthcare Gamejam/Assets/Scripts/HoverSeeThroughWalls.cs
@@ -1,61 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HoverSeeThroughWalls : MonoBehaviour
 {
-    private LayerMask wallMask;
+    [SerializeField] Material seeThroughMaterial;
+    Material myMaterial;
+    private void Awake()
+    {
+        myMaterial = gameObject.GetComponent<MeshRenderer>().material;
+    }
+    private void OnMouseOver()
+    {
+        //gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().material = seeThroughMaterial;
+
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        Invoke("RestartCollider", 1f);
+    }
+    // has to be restarted like this or it will jump on/off unstoppable
+    void RestartCollider()
+    {
+        //gameObject.GetComponent<Renderer>().enabled = true;
+        gameObject.GetComponent<MeshRenderer>().material = myMaterial;
+
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+}
+
+
+
+//***************************************
+
+
+/*
+   Debug.Log("mouseIsOver");
+   Vector2 cutoutPos = mainCamera.WorldToViewportPoint(myPos);
+   Debug.Log("step 1 reached");
+
+   cutoutPos.y /= (Screen.width / Screen.height);
+   Debug.Log("step 2 reached");
+
+   Vector3 offset = myPos - cameraObject.transform.position;
+   Debug.Log("step 3 reached");
+
+   RaycastHit[] hitObjects = Physics.RaycastAll(cameraObject.transform.position, offset, offset.magnitude, wallMask);
+   Debug.Log("step 4 reached");
+
+   for (int i = 0; i < hitObjects.Length; ++i)
+   {
+       Debug.Log("step 5 reached");
+
+       Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+       Debug.Log("step 6 reached");
+
+       for (int m = 0; m < materials.Length; ++m)
+       {
+           Debug.Log("step 7 reached");
+
+           materials[m].SetVector("_CutoutPos", cutoutPos);
+           materials[m].SetFloat("_CutoutSize", 0.1f);
+           materials[m].SetFloat("_FalloffSize", 0.05f);
+           Debug.Log("step 8 reached");
+       }
+       Debug.Log("step 9 reached");
+   }
+   Debug.Log("step 10 reached");*/
+
+
+/*private void OnMouseExit()
+{
+    gameObject.GetComponent<Renderer>().enabled = true;
+    //gameObject.GetComponent<BoxCollider>().enabled = true;
+
+    //gameObject.SetActive(true);
+}*/
+
+/*private LayerMask wallMask;
+    private GameObject cameraObject;
     private Camera mainCamera;
     private Vector3 myPos;
 
     private void Awake()
     {
-        mainCamera = GetComponent<Camera>();
+        //cameraObject = GameObject.Find("Main Camera");
+        //mainCamera = cameraObject.GetComponent<Camera>();
         myPos = gameObject.transform.position;
-    }
-
-    private void OnMouseOver()
-    {
-        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(myPos);
-        cutoutPos.y /= (Screen.width / Screen.height);
-        Vector3 offset = myPos - mainCamera.transform.position;
-        RaycastHit[] hitObjects = Physics.RaycastAll(mainCamera.transform.position, offset, offset.magnitude, wallMask);
-
-        for (int i = 0; i < hitObjects.Length; ++i)
-        {
-            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
-
-            for (int m = 0; m < materials.Length; ++m)
-            {
-                materials[m].SetVector("_CutoutPos", cutoutPos);
-                materials[m].SetFloat("_CutoutSize", 0.1f);
-                materials[m].SetFloat("_FalloffSize", 0.05f);
-            }
-        }
-    }
-    /*
-    private void Update()
-    {
-        //add this:
-        RaycastHit hit;
-        //mousePosition = hit;
-
-
-        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(myPos);
-        cutoutPos.y /= (Screen.width / Screen.height);
-        Vector3 offset = myPos - mainCamera.transform.position;
-        RaycastHit[] hitObjects = Physics.RaycastAll(mainCamera.transform.position, offset, offset.magnitude, wallMask);
-
-        for (int i = 0; i < hitObjects.Length; ++i)
-        {
-            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
-
-            for (int m = 0; m < materials.Length; ++m)
-            {
-                materials[m].SetVector("_CutoutPos", cutoutPos);
-                materials[m].SetFloat("_CutoutSize", 0.1f);
-                materials[m].SetFloat("_FalloffSize", 0.05f);
-            }
-        }
     }*/
-}
