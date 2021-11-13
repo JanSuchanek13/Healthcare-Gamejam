68e52efc1e440d190bce25339731ff5373f57ef2
Uthak <81364780+Uthak@users.noreply.github.com>
2021-11-13 19:48:26 +0100

    Felix Update
    built level further
    easter eggs
    player character
    navMesh update

--- Healthcare Gamejam/Assets/Scripts/CharacterController.cs
+++ Healthcare Gamejam/Assets/Scripts/CharacterController.cs
@@ -4,19 +4,30 @@ using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
-    //public Vector3 GoToAllyPoint; // why tho
     NavMeshAgent agent;
-    //private GameObject cameraObject;
-    //private Camera mainCamera;
+    // Animation Assets on character
+    Animation anim;
+    bool isOnTheMove = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        //cameraObject = GameObject.Find("Main Camera");
-        //mainCamera = cameraObject.GetComponent<Camera>();
+        // controls animations
+        anim = gameObject.GetComponent<Animation>();
+        StartCoroutine(CheckMoving());
     }
     void Update()
     {
+        if (isOnTheMove)
+        {
+            anim.Stop("honeyArmature_IdleHoney");
+            anim.Play("honeyArmature_walkinghoney");
+        }else
+        {
+            anim.Stop("honeyArmature_walkinghoney");
+            anim.Play("honeyArmature_IdleHoney");
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -24,76 +35,97 @@ public class CharacterController : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
-            /* Debug.Log("step 1");
-
-             Transform targetTransform = null; // target assigned in inspector 
-             Debug.Log("step 2");
-
-             RaycastHit[] hits; // returned hits
-             Debug.Log("step 3");
-
-             LayerMask mask = new LayerMask(); // set in inspector
-             Debug.Log("step 4");
-
-             hits = Physics.RaycastAll(transform.position, transform.forward, Mathf.Infinity);
-             Debug.Log("step 5");
-
-             for (int i = 0; i < hits.Length; i++)
-             {
-                 Debug.Log("step 6");
-
-                 if (hits[i].collider.gameObject.CompareTag("Floor"))
-                 {
-                     Debug.Log("Found Floor!");
-
-                     // Found something with a given tag. 
-                 }
-             }
-
-             /*
-                 Debug.Log("step 1");
-
-             //RaycastHit hit;
-             RaycastHit[] hits;
-             Debug.Log("step 2");
-
-             LayerMask mask = new LayerMask();
-             hits = Physics.RaycastAll(cameraObject.transform.position, transform.forward, mask);
-             hits = Physics.RaycastAll(mainCamera.transform.position, transform.forward, Mathf.Infinity);
-             Debug.Log("step 3");
-
-
-             for (int i = 0; i < hits.Length; i++)
-             {
-                 Debug.Log("step 4");
-
-                 if (hits[i].collider.gameObject.CompareTag("Floor"))
-                 {
-                     Debug.Log("step 5");
-
-                     //agent.SetDestination(hit.point);
-                 }
-             }
-
-
-                 /*if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-                 {
-                     //GameObject hitObjct = hit.collider.gameObject;
-
-                     hit.collider.gameObject.GetComponent<Collider>().enabled = false; 
-
-                     agent.SetDestination(hit.point);
-                 }
-                 if (hit.collider.gameObject.CompareTag("Wall"))
-                 {
-                     hit.collider.gameObject.GetComponent<Collider>().enabled = false;
-                     RaycastHit newHit;
-                 }
-                 // old script
-                 if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-             {
-                 agent.SetDestination(hit.point);
-             }*/
         }
     }
+    IEnumerator CheckMoving()
+    {
+        Vector3 startPos = transform.position;
+        yield return new WaitForSeconds(.1f);
+        Vector3 finalPos = transform.position;
+
+        if (startPos != finalPos)
+        {
+            isOnTheMove = true;
+        }else
+        {
+            isOnTheMove = false;
+        }
+        StartCoroutine(CheckMoving());
+    }
 }
+
+
+//**********************************
+
+
+
+/* Debug.Log("step 1");
+
+ Transform targetTransform = null; // target assigned in inspector 
+ Debug.Log("step 2");
+
+ RaycastHit[] hits; // returned hits
+ Debug.Log("step 3");
+
+ LayerMask mask = new LayerMask(); // set in inspector
+ Debug.Log("step 4");
+
+ hits = Physics.RaycastAll(transform.position, transform.forward, Mathf.Infinity);
+ Debug.Log("step 5");
+
+ for (int i = 0; i < hits.Length; i++)
+ {
+     Debug.Log("step 6");
+
+     if (hits[i].collider.gameObject.CompareTag("Floor"))
+     {
+         Debug.Log("Found Floor!");
+
+         // Found something with a given tag. 
+     }
+ }
+
+ /*
+     Debug.Log("step 1");
+
+ //RaycastHit hit;
+ RaycastHit[] hits;
+ Debug.Log("step 2");
+
+ LayerMask mask = new LayerMask();
+ hits = Physics.RaycastAll(cameraObject.transform.position, transform.forward, mask);
+ hits = Physics.RaycastAll(mainCamera.transform.position, transform.forward, Mathf.Infinity);
+ Debug.Log("step 3");
+
+
+ for (int i = 0; i < hits.Length; i++)
+ {
+     Debug.Log("step 4");
+
+     if (hits[i].collider.gameObject.CompareTag("Floor"))
+     {
+         Debug.Log("step 5");
+
+         //agent.SetDestination(hit.point);
+     }
+ }
+
+
+     /*if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+     {
+         //GameObject hitObjct = hit.collider.gameObject;
+
+         hit.collider.gameObject.GetComponent<Collider>().enabled = false; 
+
+         agent.SetDestination(hit.point);
+     }
+     if (hit.collider.gameObject.CompareTag("Wall"))
+     {
+         hit.collider.gameObject.GetComponent<Collider>().enabled = false;
+         RaycastHit newHit;
+     }
+     // old script
+     if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+ {
+     agent.SetDestination(hit.point);
+ }*/
